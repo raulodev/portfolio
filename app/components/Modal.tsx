75f8df6df19233729ab2af0689a93dbd7c23a3b0
raulodev <raulodev@gmail.com>
2024-12-07 19:24:49 -0500

    update

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -11,9 +11,14 @@ interface ModalProps {
 export const Modal: React.FC<ModalProps> = ({ onClose, project }) => {
   return (
     <motion.div
+      id="overlay"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
+      onClick={(event) => {
+        const target = event.target as HTMLDivElement;
+        if (target.id === "overlay") onClose();
+      }}
       className="fixed backdrop-blur top-0 right-0 bottom-0 left-0 bg-black bg-opacity-10 flex justify-center items-center"
     >
       <div className="bg-white w-full self-end sm:self-center sm:w-3/4 lg:w-1/2  p-4 rounded-t sm:rounded  shadow-md space-y-2">
